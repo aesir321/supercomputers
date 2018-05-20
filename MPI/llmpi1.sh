7cd43486b6a76737b782011e7d05130389fa0d00
Will Parker <parkerwill625@gmail.com>
2018-05-20 10:00:09 +0200

    Assignment 2.

---
+++ MPI/llmpi1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#@ wall_clock_limit = 00:10:00
+#@ job_name = pos-lulesh-openmp
+#@ job_type = MPICH
+#@ class = test
+#@ initialdir = $(home)/programming-of-supercomputers/assignment_1
+#@ output = data/task5.2/pos_lulesh_mpi_phase1_$(jobid).out
+#@ error = data/task5.2/pos_lulesh_mpi_phase1_$(jobid).out
+#@ node = 1
+#@ total_tasks = 16
+#@ node_usage = not_shared
+#@ energy_policy_tag = lulesh
+#@ minimize_time_to_solution = yes
+#@ island_count = 1
+#@ queue
+
+. /etc/profile
+. /etc/profile.d/modules.sh
+. $HOME/.bashrc
+
+module unload mpi.ibm
+module load mpi.intel
+module load intel/16.0
+module list
+
+mpiexec -n 1 ./code/task5.2/lulesh2.0/lulesh2.0
+mpiexec -n 8 ./code/task5.2/lulesh2.0/lulesh2.0
